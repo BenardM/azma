46d050a2c67de1e5e840ba8eab0854429de3535d
BenardM <maxime.benardb2b@gmail.com>
2017-12-20 22:04:32 +0100

    Route for index home

--- Modules/Home/Resources/views/index.blade.php
+++ Modules/Home/Resources/views/index.blade.php
@@ -1,9 +1,9 @@
 @extends('home::layouts.master')
 
 @section('content')
-    <h1>Hello World</h1>
+    <h1>AZMA</h1>
 
     <p>
-        This view is loaded from module: {!! config('home.name') !!}
+        hello azma !
     </p>
 @stop
